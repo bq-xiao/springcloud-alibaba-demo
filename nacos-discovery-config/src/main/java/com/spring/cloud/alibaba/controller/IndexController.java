2deae83199ba853d02d08686e6d47a141a5db7be
dmd <dmd@mac.com>
2020-11-09 16:21:12 +0800

    refactor

--- nacos-discovery-config/src/main/java/com/spring/cloud/alibaba/controller/IndexController.java
+++ nacos-discovery-config/src/main/java/com/spring/cloud/alibaba/controller/IndexController.java
@@ -1,11 +1,10 @@
 package com.spring.cloud.alibaba.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -16,6 +15,8 @@ import java.net.UnknownHostException;
 public class IndexController {
     @Value("${common.message}")
     private String message;
+    @Autowired
+    private Environment environment;
 
     @RequestMapping(value = "/echo", method = RequestMethod.GET)
     @ResponseBody
@@ -28,5 +29,11 @@ public class IndexController {
     @ResponseBody
     public String value() {
         return message;
+    }
+
+    @RequestMapping(value = "/value/{key}", method = RequestMethod.GET)
+    @ResponseBody
+    public String value(@PathVariable String key) {
+        return environment.getProperty(key, "this is default!");
     }
 }
